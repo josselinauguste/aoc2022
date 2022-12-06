bd3c0e41ce91a6d8fb914b7bf0b41655251ae080
Josselin Auguste <jauguste@iblop.net>
2022-12-06 22:39:33 +0100

    Day 5

---
+++ src/main/kotlin/Day5.kt
@@ -0,0 +1,34 @@
+import java.util.*
+
+fun cratesOnTopOfStacks(input: String): String {
+    val (rawStacks, rawProcedure) = input.split("\n\n")
+    val stacks = parseStacks(rawStacks)
+    val procedure = parseProcedure(rawProcedure)
+    for (p in procedure) {
+        for (i in 1..p.first) {
+            stacks[p.third - 1].push(stacks[p.second - 1].pop())
+        }
+    }
+    return stacks.map { s -> s.last() }.joinToString("")
+}
+
+fun parseStacks(raw: String): List<Stack<Char>> {
+    val storeys = raw.split("\n").reversed().drop(1)
+    val stacks: MutableList<Stack<Char>> = mutableListOf()
+    for (storey in storeys) {
+        for (crateIndex in 1..storey.length step 4) {
+            val stackIndex = (crateIndex - 1) / 4
+            if (stacks.size == stackIndex) {
+                stacks.add(Stack())
+            }
+            val crate = storey[crateIndex]
+            if (crate != ' ') stacks[stackIndex].push(crate)
+        }
+    }
+    return stacks
+}
+
+fun parseProcedure(rawProcedure: String): List<Triple<Int, Int, Int>> =
+    Regex("move (\\d+) from (\\d+) to (\\d+)").findAll(rawProcedure)
+        .map { m -> val (_, m1, m2, m3) = m.groupValues; Triple(m1.toInt(), m2.toInt(), m3.toInt()) }
+        .toList()
